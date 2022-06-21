391b6f93ed7641fc94e454a6c075d8a83ae1a11d
carver <lijialong8012@163.com>
2022-06-21 20:16:38 +0800

    fix

--- handler/login.go
+++ handler/login.go
@@ -1,17 +1,20 @@
 package handler
 import (
 "github.com/gin-gonic/gin"
-"io/ioutil"
-"net/http"
-//"fmt"
+//"io/ioutil"
+//"net/http"
+. "fmt"
+_"carver_golang_blog/config"
 )
+
 
 //用户登录
 func Login(context *gin.Context) {
     //获取请求body
-    user_params, _ := ioutil.ReadAll(context.Request.Body)
-    user_info := string(user_params)
-    context.JSON(http.StatusOK, gin.H{
-	"message": user_info,
-    })
+//    user_params, _ := ioutil.ReadAll(context.Request.Body)
+//    user_info := string(user_params)
+    Println("你好")
+//    context.JSON(http.StatusOK, gin.H{
+//	"message": user_info,
+//    })
 }
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 import "carver_golang_blog/router"
-func main(){
 
+func main(){
     //初始化go-gin，启动webserver
     r := router.CarverRouter()
     _ = r.Run(":8012")
