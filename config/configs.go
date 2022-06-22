15471ac3e09cef31dcbaa1dd38d04a2d35f9cc2f
carver <lijialong8012@163.com>
2022-06-22 14:55:50 +0800

    fix(添加数据库配置)

--- config/configs.go
+++ config/configs.go
@@ -6,33 +6,65 @@ import (
 	 _"github.com/go-sql-driver/mysql"
 	"github.com/BurntSushi/toml"
 	//"path/filepath"
+	"os"
 )
 
 type tomlConfig struct {
-	Title   string
-	Api  	apiInfo
-	Db      database `toml:"database.main"`
+	Title       string
+	Admin       adminInfo
+	Db          database    `toml:"database"`
+	Redis       redisInfo
+	Servers     map[string]server
+	Other       other
 }
 
-type apiInfo struct {
-	ListenPort  string
-	Debug  bool
-	LogLevel  string
+type adminInfo struct {
+	Name        string
+	Port        int     `toml:"ListenPort"`
+	Debug       bool
+	LogLevel    string
+
 }
 
 type database struct {
-	DSN  string
-	Active  string
+    UserName    string  `toml:"username"`
+    Password    string
+    Database    string
+	Addr        string
+	Ports       []int
+	ConnMax     int     `toml:"connection_max"`
+	Enabled     bool
+	Port        int     `toml:"system_port"`
 }
+
+type redisInfo struct {
+    Addr        string
+    Auth        string
+    Dial        string  `toml:"DialTimeout"`
+    Read        string  `toml:"ReadTimeout"`
+    Write       string  `toml:"WriteTimeout"`
+}
+
+
+type server struct {
+	Db      string
+	Pwd     string
+}
+
+type other struct {
+	Data    [][]interface{}
+	Type    []string
+}
+
 
 
 func init() {
 
     pwd, err_pwd := os.Getwd()
-        if err_pwd != nil {
-             Println(err_pwd)
-             os.Exit(1)
-        }
+    if err_pwd != nil {
+         Println(err_pwd)
+         os.Exit(1)
+    }
 
     env_url := pwd + "/config/local.config.toml"
 
@@ -44,10 +76,8 @@ func init() {
     	return
     }
 
-
-
 	//"用户名:密码@[连接方式](主机名:端口号)/数据库名"
-	db, _ := sql.Open("mysql", "用户名:密码@(localhost)/数据库") // 设置连接数据库的参数
+	db, _ := sql.Open("mysql", config.Db.UserName+":"+config.Db.Password+"@(localhost)/"+config.Db.Database) // 设置连接数据库的参数
 	defer db.Close()                                            //关闭数据库
 	err := db.Ping()                                            //连接数据库
 	if err != nil {
